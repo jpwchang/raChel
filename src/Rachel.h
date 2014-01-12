526ed70ddde87613e14341719a9a6d9f7c361206
Jonathan Chang <jonathan.chang13@gmail.com>
2014-01-12 15:13:48 -0800

    While loops

--- src/Rachel.h
+++ src/Rachel.h
@@ -1,7 +1,7 @@
 /**
  * The raChel programming language - a Rachelisms wrapper for C++
  * By Jonathan Chang
- * Requires a C++ 11 compiler, like Visual Studio 2012+
+ * Requires a C++ 11 compiler, like Visual Studio 2013+
  */
 
 #define LET_ME_TELL_YOU int
@@ -21,5 +21,8 @@
 #define BUT_ACTUALLY_THOUGH else
 #define I_FEEL_LIKE for(
 #define GREW_UP ++
+#define I_HAVE_SOME_BAD_NEWS_FOR --
 #define THAT_KID int
 #define AND_MAYBE ;
+#define DO_YOU_BUY while(
+#define BRO )
